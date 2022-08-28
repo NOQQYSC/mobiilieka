cc6cc33d794879e2ea2090a53b86e2d8ba7bc5ae
Kemi <kembalos@hotmail.com>
2022-08-29 00:26:30 +0300

    valmis

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, keyboardType } from 'react-native';
 
 export default function App() {
+
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  const plus = () => {
+    setResult(result => parseInt(number1) + parseInt(number2))
+  }
+
+  const minus = () => {
+    setResult(result => number1 - number2)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Testi</Text>
+      <Text>Result: {result}</Text>
+      <View style={{flexDirection:"column", justifyContent: 'space-around'}}>
+      <TextInput style={{width: 200, borderBottomColor: 'gray', borderWidth: 1}}
+       onChangeText={numba1 => setNumber1(numba1)} value={number1} keyboardType= 'numeric'/>
+      <TextInput style={{width: 200, borderBottomColor: 'gray', borderWidth: 1}}
+       onChangeText={numba2 => setNumber2(numba2)} value={number2} keyboardType= 'numeric'/>
+       </View>
+       <View style={{flexDirection:"row", justifyContent: 'space-around'}}>
+          <Button onPress={plus} title="+"/>
+          <Button onPress={minus} title="-"/>
+
+       </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -14,6 +40,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'space-between',
     alignItems: 'center',
     justifyContent: 'center',
   },
